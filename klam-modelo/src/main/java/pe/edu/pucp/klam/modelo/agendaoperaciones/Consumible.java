7273bbc1abbb27d32254e22380f9d9498d3c64ac
madi11uwu <madison.obregon@pucp.edu.pe>
2026-09-11 17:14:06 -0500

    agendaoperaciones

---
+++ klam-modelo/src/main/java/pe/edu/pucp/klam/modelo/agendaoperaciones/Consumible.java
@@ -0,0 +1,5 @@
+package pe.edu.pucp.klam.modelo.agendaoperaciones;
+
+public class Consumible {
+
+}
---
+++ klam-modelo/src/main/java/pe/edu/pucp/klam/modelo/agendaoperaciones/Equipo.java
@@ -0,0 +1,11 @@
+package pe.edu.pucp.klam.modelo.agendaoperaciones;
+
+public class Equipo {
+    private String id_equipo;
+    private String nombre;
+    //CategoriaEquipo categoria
+    //Map<String,Object> especificaciones
+    private boolean disponible;
+    private boolean activo;
+
+}
