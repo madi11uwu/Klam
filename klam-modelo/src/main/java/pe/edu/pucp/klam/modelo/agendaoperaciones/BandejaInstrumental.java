b53b3b6c28b54cd12dfc820edcd5936a778bbdf6
madi11uwu <madison.obregon@pucp.edu.pe>
2026-09-11 17:22:23 -0500

    agendaoperaciones

--- klam-modelo/src/main/java/pe/edu/pucp/klam/modelo/agendaoperaciones/BandejaInstrumental.java
+++ klam-modelo/src/main/java/pe/edu/pucp/klam/modelo/agendaoperaciones/BandejaInstrumental.java
@@ -1,5 +1,14 @@
 package pe.edu.pucp.klam.modelo.agendaoperaciones;
 
-public class BandejaInstrumental {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BandejaInstrumental {
+    private int id_banceja;
+    private String tipo;
+    private boolean esterilizado;
+    private List<Consumible> consumbiles;
+    public BandejaInstrumental() {
+        consumbiles=new ArrayList<>();
+    }
 }
--- klam-modelo/src/main/java/pe/edu/pucp/klam/modelo/agendaoperaciones/Consumible.java
+++ klam-modelo/src/main/java/pe/edu/pucp/klam/modelo/agendaoperaciones/Consumible.java
@@ -1,5 +1,8 @@
 package pe.edu.pucp.klam.modelo.agendaoperaciones;
 
 public class Consumible {
-
+    private int id_consumible;
+    private String nombreComercial;
+    private String marca;
+    private String medida;
 }
