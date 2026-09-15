0d087e8165d13cffadf28242e17c2f6b12608c25
Miguel <noisetgoud@gmail.com>
2026-09-15 01:04:45 -0500

    Interfaz Facturable y enums de estado del modulo financiero

---
+++ klam-app/src/main/java/pe/edu/pucp/app/PruebaFacturacion.java
@@ -0,0 +1,4 @@
+package pe.edu.pucp.app;
+
+public class PruebaFacturacion {
+}
---
+++ klam-modelo/src/main/java/pe/edu/pucp/klam/modelo/documentacionfinanzas/EstadoPago.java
@@ -0,0 +1,7 @@
+package pe.edu.pucp.klam.modelo.documentacionfinanzas;
+
+public enum EstadoPago {
+    PENDIENTE,
+    PAGADO,
+    ANULADO
+}
